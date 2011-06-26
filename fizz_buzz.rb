f339ae5ea0f36047b463d362007cc901f1183235
Mark Burns <markthedeveloper@googlemail.com>
2011-06-26 12:47:38 +0100

    Further classerisationificationment

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,15 +1,3 @@
-#Every 5th number say Fizz. Every 3rd say Buzz. Both say FizzBuzz
-usage = <<EOF
-  Usage:
-
-  ruby fizz_buzz.rb <value>
-
-  <value> is the number to rub Fizz Buzz up to
-EOF
-
-max_value = ARGV[0].to_i
-puts usage unless max_value > 0
-
 class Fizzer
   attr_accessor :number
   def initialize number
@@ -24,14 +12,37 @@ class Buzzer
   end
 end
 
-(1..max_value).each do |i|
-  fizzer = Fizzer.new i
-  buzzer = Buzzer.new i
-  fizz = fizzer.number % 5 == 0
-  buzz = buzzer.number % 3 == 0
+class FizzBuzzPrinter
+  def initialize max_value
+    (1..max_value).each do |i|
+      fizzer = Fizzer.new i
+      buzzer = Buzzer.new i
+      fizz = fizzer.number % 5 == 0
+      buzz = buzzer.number % 3 == 0
 
-  puts("#{i}: ".tap do |m|
-    m << "Fizz" if fizz
-    m << "Buzz" if buzz
-  end)
+      puts("#{i}: ".tap do |m|
+        m << "Fizz" if fizz
+        m << "Buzz" if buzz
+      end)
+    end
+  end
 end
+
+class Main
+  def self.run! max
+    FizzBuzzPrinter.new max
+  end
+end
+
+usage = <<EOF
+  Usage:
+
+  ruby fizz_buzz.rb <value>
+
+  <value> is the number to rub Fizz Buzz up to
+EOF
+
+max_value = ARGV[0].to_i
+puts usage unless max_value > 0
+
+Main.run! max_value
