474f7e67ebe14b553ea8feb95d89df512a5d8adf
Mark Burns <markthedeveloper@googlemail.com>
2011-06-26 12:41:37 +0100

    Added usage message

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,11 +1,21 @@
 #Every 5th number say Fizz. Every 3rd say Buzz. Both say FizzBuzz
+usage = <<EOF
+  Usage:
 
-(1..ARGV[0].to_i).each do |i|
+  ruby fizz_buzz.rb <value>
+
+  <value> is the number to rub Fizz Buzz up to
+EOF
+
+max_value = ARGV[0].to_i
+puts usage unless max_value > 0
+
+(1..max_value).each do |i|
   fizz = i % 5 == 0
   buzz = i % 3 == 0
-  message = "#{i}: ".tap do |m|
+
+  puts("#{i}: ".tap do |m|
     m << "Fizz" if fizz
     m << "Buzz" if buzz
-  end
-  puts message
+  end)
 end
